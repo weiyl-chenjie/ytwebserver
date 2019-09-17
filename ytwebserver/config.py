842fd02d237b55cb1b8e7488b3819dab2bf8bde1
peach-weiyl <nbzdds08@163.com>
2019-09-18 07:06:12 +0800

    收集静态文件

--- ytwebserver/config.py
+++ ytwebserver/config.py
@@ -1,7 +1,8 @@
 import platform
 if platform.system() == 'Windows':
     DATABASE_PASSWORD_default = 'it-profi'
-    HOST_default = '10.40.254.196'
+    # HOST_default = '10.40.254.196'
+    HOST_default = '192.168.0.103'
     EMAIL_HOST = '10.40.3.8'
     EMAIL_HOST_USER = 'yulin.wei@huf-group.com'
     ADMINS = [('尉玉林', 'yulin.wei@huf-group.com')]
